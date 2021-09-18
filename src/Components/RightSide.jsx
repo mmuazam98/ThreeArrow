8b191aaa5b31c9a5ac30e0323e6275651e6e6e1d
Mohammad Muazam <mm9084@srmist.edu.in>
2021-09-18 18:16:52 +0530

    Pushed

--- src/Components/RightSide.jsx
+++ src/Components/RightSide.jsx
@@ -6,6 +6,16 @@ const RightSide = () => {
       <div className="sub-heading">
         <p>Generate AI First Line</p>
       </div>
+      <div className="content">
+        <p className="content-heading">Personalised Message 1</p>
+        <div className="content-desc">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis rerum modi exercitationem dolores blanditiis suscipit nulla, voluptas illo nostrum debitis doloribus recusandae itaque
+          vitae soluta eos quo quidem ducimus nesciunt ipsa in, ab quasi unde totam rem?
+          <div className="link">
+            <a href="#Link">Link</a> to temporary site
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
